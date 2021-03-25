adb08fd9ed38f3c24edb662e5fb65faaedcb545e
Lethaldrien <voulgarakisromanos@gmail.com>
2021-03-25 13:57:33 +0200

    Preliminary addition of SGP4 and IGRF.

---
+++ cmake-build-debug/CMakeFiles/on_board_software.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/on_board_software.dir/test.cpp.o"
+  "on_board_software"
+  "on_board_software.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/on_board_software.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
