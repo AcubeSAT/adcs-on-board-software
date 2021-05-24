0c9f34c85c76381a1eebb80380f667527f8d3992
Georgios Savvidis <giorgossav22@gmail.com>
2021-05-24 14:20:37 +0000

    Added IGRF, some required functions and the corresponding tests

---
+++ test/date2decimalTest.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch.hpp>
+#include "mathFunctions.h"
+
+TEST_CASE("Date to decimal date test") {
+    REQUIRE(date2decimal(2024, 1, 1, 0, 0, 0) == Approx(2024).epsilon(0.000001));
+    REQUIRE(date2decimal(2024, 12, 32, 0, 0, 0) == Approx(2025).epsilon(0.000001));
+    REQUIRE(date2decimal(2024, 7, 24, 7, 23, 28) == Approx(2024.5609507185).epsilon(0.000001));
+    REQUIRE(date2decimal(2024, 1, 10, 11, 54, 26) == Approx(2024.0259457220).epsilon(0.000001));
+    REQUIRE(date2decimal(2024, 9, 11, 9, 27, 16) == Approx(2024.6950653967).epsilon(0.000001));
+    REQUIRE(date2decimal(2024, 5, 30, 10, 52, 58) == Approx(2024.4110749975).epsilon(0.000001));
+    REQUIRE(date2decimal(2023, 6, 9, 6, 9, 6) == Approx(2023.4363186834).epsilon(0.000001));
+    REQUIRE(date2decimal(2100, 6, 9, 6, 9, 6) == Approx(2100.4363186834).epsilon(0.000001));
+    REQUIRE(date2decimal(2400, 9, 11, 9, 27, 16) == Approx(2400.6950653967).epsilon(0.000001));
+}
