8d4d92a71a068204195acb70e28dcd7367b4b660
Valia Pappa <valia.pp8@gmail.com>
2021-05-27 15:11:57 +0300

    Lower epsilon in the tests

--- test/AlbedoTest.cpp
+++ test/AlbedoTest.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Calculate cell area test") {
     int16_t j = 50;
 
     float cellArea = calculateCellArea(i, j);
-    REQUIRE(cellArea == Approx(1.003737674791411e+10).epsilon(0.01));
+    REQUIRE(cellArea == Approx(1.003737674791411e+10).epsilon(1e-4));
 }
 
 TEST_CASE("Calculate grid area test") {
@@ -19,7 +19,7 @@ TEST_CASE("Calculate grid area test") {
     int8_t sunIndexJ = 95;
 
     float gridAngleResult = gridAngle(loopI, loopJ, sunIndexI, sunIndexJ);
-    REQUIRE(gridAngleResult == Approx(1.442580854116471).epsilon(0.01));
+    REQUIRE(gridAngleResult == Approx(1.442580854116471).epsilon(1e-4));
 }
 
 TEST_CASE("Indices to radians test") {
@@ -27,19 +27,19 @@ TEST_CASE("Indices to radians test") {
     int16_t j = 50;
 
     Vector2f radians = albedo::indicesToRadians(i, j);
-    REQUIRE(radians(0) == Approx(-2.039853563268373).epsilon(0.01));
-    REQUIRE(radians(1) == Approx(2.434734306532090).epsilon(0.01));
+    REQUIRE(radians(0) == Approx(-2.039853563268373).epsilon(1e-4));
+    REQUIRE(radians(1) == Approx(2.434734306532090).epsilon(1e-4));
 }
 
 TEST_CASE("Radians to indices test") {
     Vector<int16_t, 2> indices1 = radiansToIndices(20, 40);
-    REQUIRE(indices1(0) == Approx(-2110).epsilon(0.01));
-    REQUIRE(indices1(1) == Approx(1060).epsilon(0.01));
+    REQUIRE(indices1(0) == Approx(-2110).epsilon(1e-2));
+    REQUIRE(indices1(1) == Approx(1060).epsilon(1e-2));
 
     Vector<int16_t, 2> indices2 = radiansToIndices(1.0e+06 * 0.000000674740942,
                                                    1.0e+06 * 0.000001446501385);
-    REQUIRE(indices2(0) == Approx(97).epsilon(0.01));
-    REQUIRE(indices2(1) == Approx(174).epsilon(0.01));
+    REQUIRE(indices2(0) == Approx(97).epsilon(1e-4));
+    REQUIRE(indices2(1) == Approx(174).epsilon(1e-4));
 }
 
 TEST_CASE("Albedo test") {
@@ -51,8 +51,8 @@ TEST_CASE("Albedo test") {
 
 
     Matrix<float, 180, 288> albedo1 = calculateAlbedo(satellite, sunPosition, reflectivityData1);
-    REQUIRE(albedo1.sum() == Approx(0.065217631207925).epsilon(0.01));
+    REQUIRE(albedo1.sum() == Approx(0.065217631207925).epsilon(1e-4));
 
     Matrix<float, 180, 288> albedo2 = calculateAlbedo(satellite, sunPosition, reflectivityData2);
-    REQUIRE(albedo2.sum() == Approx(0.003260881560396).epsilon(0.01));
+    REQUIRE(albedo2.sum() == Approx(0.003260881560396).epsilon(1e-4));
 }
