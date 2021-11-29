e48b02651ecdd06c742b5fe39f87aaef1fe4a8fa
Manolis Mylonas <mylowade98@gmail.com>
2021-11-29 17:58:26 +0000

    Wahba updated

---
+++ inc/wahba.h
@@ -0,0 +1,20 @@
+#ifndef ADCS_ONBOARD_SOFTWARE_WAHBA_H
+#define ADCS_ONBOARD_SOFTWARE_WAHBA_H
+
+#include "Eigen/Geometry"
+#include "SatelliteModel.h"
+
+/**
+ * Function that solves Wahba's problem with the use of the SVD Algorithm. The two vectors are the magnetic field and
+ * the sun vector, while the two frames are Body and ECI.
+ * @param magneticBody the magnetic field vector in Body Frame
+ * @param magneticECI the magnetic field vector in ECI - Reference Frame
+ * @param sunBody the sun vector in Body Frame
+ * @param sunECI the sun vector in ECI - Reference Frame
+ * @return quaternion from Body frame to ECI
+ */
+Eigen::Quaternionf
+wahba(Eigen::Vector3f magneticBody, Eigen::Vector3f magneticECI, Eigen::Vector3f sunBody, Eigen::Vector3f sunECI);
+
+#endif //ADCS_ONBOARD_SOFTWARE_WAHBA_H
+
