652bee6e38f3ad25b4b336f73ff3c928304158a2
xlxs4 <orousoultzoglou@gmail.com>
2021-12-09 21:22:43 +0200

    AFL++ Integration
    gitignore: only ignore the executables in root dir
    
    Initial commit
    
    afl: correctly use docker volume in README
    
    afl: highlight advantages of docker volume in README
    
    afl: Add example screenshots in README
    
    afl: Fix README pictures
    
    afl: use laf-intel for instrumentation
    
    afl: add sanitizers support
    
    afl: tell CMake to use afl-c++
    
    afl: set AR and RANLIB to llvm for lto
    
    afl: explicitly use afl-c++ instead of afl-cc
    
    afl: add todos in README
    
    Tell CMake to add STATIC libraries
    
    afl: Make dummy test crashing
    
    cmake: remove STATIC
    
    afl: Tailor dummy test crash behavior
    
    afl: Add afl-cmin shellscripts
    
    afl: afl-cmin script fixes
    
    afl: Add resources in README
    
    afl: use multiple fuzzer instances
    
    afl: helper scripts tideup
    
    afl: add shell directive, wrap mkdir/cd
    
    afl: send ctrl+c to screen before killing it
    
    afl: document helper scripts
    
    afl: Add ToC in README
    
    afl: Add Using section in README
    
    afl: Add TL;DR section in README
    
    afl: use different script to abort afl-fuzz and to kill screen sessions
    
    afl: Explain shell scripts in README
    
    afl: fix stop-fuzz.sh
    
    afl: Add how to attach/detach to screen in README
    
    afl: Embed TL;DR asciinema recording in README
    
    afl: Better asciinema rec in README
    
    afl: Use afl-tmin to minimize initial testcases
    
    afl: document afl-tmin helper script
    
    afl: untangle commands in helper scripts
    
    afl: Parallelize afl-tmin
    
    afl: Split instrumentation and fuzzing in separate scripts
    
    afl: Support afl-tmin on the afl-cmin minimized corpus
    
    afl: Use cp instead of rsync in re-tmin
    
    To avoid a nasty nasty _resume directory cleanup failed bug that I caused because I'm an idiot and it's late.
    Here is what generates the error in case you were interested:
    https://github.com/mirrorer/afl/blob/master/afl-fuzz.c#L3581
    
    afl: Mention screen caveat in README
    
    afl: Add clean script
    
    afl: Record final asciinema cast
    
    afl: Use function to install packages in setup.sh
    
    afl: Add GDB and Go dependencies
    
    afl: Setup exploitable GDB pluging and crashwalk
    
    afl: Use cwtriage and cwdump to triage found crashes
    
    afl: Add TODO roadmap in README
    
    afl: Remove obsolete instrumentation section from README
    
    afl: Update clean.sh
    
    afl: Don't return fuzz in main; prepare persistent mode
    
    afl: Parse cleanup
    Thanks to template argument deduction and uniform initialization
    
    afl: Deferred initialization
    
    afl: Implement persistent mode
    
    afl: Add shared memory fuzzing TODO
    
    afl: Create separate .gitignore for afl++ dir
    
    afl: Collapse ToC in README
    
    afl: Move scripts to scripts directory
    
    afl: Update scripts path in README
    
    afl: Update asciinema rec
    
    Disable deferred initialization and persistent mode
    
    Move clean script to util directory
    
    Manually install go
    
    EditorConfig: shell configuration
    
    Add shell format utility script
    
    Format shell scripts
    
    Make formatting shellscript executable
    
    README: Mention script caveat
    
    readme: Add more TODOs
    
    afl: Add GitLab TOC in README
    
    afl: Remove redundant CXX var from instrument
    
    afl: README typo fix
    
    afl: README alignment fix
    
    afl: rename re-tmin to retmin
    
    afl: Use bash interpreter from env in shell directives
    
    afl: remove && from quitting screen session commands
    
    afl: Use screen -xr instead of -r
    
    afl: Mention manually sourcing ~/.bashrc
    
    afl: Correctly add go to PATH in setup.sh
    
    afl: Set GOPATH; add it to PATH
    
    afl: Link to main afl++ README in README
    
    afl: Mention iterative fuzzing in README
    
    afl: Mention setup.sh works inside container in README
    
    afl: remove apt autoremove from setup.sh
    
    afl: add progress bar in rsync call
    
    afl: Add RelWithDebInfo in instrument.sh
    
    afl: Remove TODOs section from README
    
    afl: Mention sourcing .bashrc in README
    
    afl: Mention GNU parallel in tmin
    
    afl: Mention screen -dr in README
    
    afl: Ensure running scripts from aflplusplus/

---
+++ aflplusplus/scripts/cmin.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Wrapper script to call the afl-cmin script in a screen session.
+# TODO: find a better way?
+
+DESIRED_PATH="aflplusplus"
+[[ "${PWD##*/}" == "$DESIRED_PATH" ]] || { echo "Run from $DESIRED_PATH"; exit; }
+
+screen -xr "cmin" -X stuff $'./scripts/_cmin.sh\n'
---
+++ aflplusplus/scripts/setup.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# Make sure all dependencies are installed and ready to go.
+
+DESIRED_PATH="aflplusplus"
+[[ "${PWD##*/}" == "$DESIRED_PATH" ]] || { echo "Run from $DESIRED_PATH"; exit; }
+
+# Have everything in the Docker container use vim instead of Joe.
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+# Check if package is installed, install it otherwise.
+function maybe-install {
+  if ! dpkg-query -W -f='${Status}' "$1" | grep "ok installed"; then
+    apt install --yes --no-install-recommends "$1"
+  fi
+}
+
+# Check if directory exists and is not empty.
+function dir-ok {
+  if [[ -d "$1" && -n "$(ls -A "$1")" ]]; then
+    true
+  else
+    false
+  fi
+}
+
+apt update \
+  && apt upgrade -y \
+  && maybe-install screen \
+  && maybe-install rsync \
+  && maybe-install gdb \
+  && maybe-install curl
+
+# Manually install go, to get a more recent version.
+# If we don't, we can't use go install, and we can't
+# use shfmt.
+
+if ! dir-ok /usr/local/go; then
+  mkdir -p ~/.go
+  rm -rf ~/go
+  GOPATH="$HOME"/.go
+  echo "GOPATH=$HOME/.go" >>"$HOME"/.bashrc \
+    && echo "export GOPATH" >>"$HOME"/.bashrc \
+    && echo "PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >>"$HOME"/.bashrc
+
+  curl -OL https://go.dev/dl/go1.17.6.linux-amd64.tar.gz \
+    && tar -C /usr/local -xvf go1.17.6.linux-amd64.tar.gz \
+    && rm -rf go1.17.6.linux-amd64.tar.gz
+
+  # A hack to be able to source .bashrc from inside the shell script.
+  # See https://askubuntu.com/a/77053/1165092
+
+  # Note that the terminal will not reflect the env changes sourcing .bashrc caused.
+  # In other words, you still have to
+  # either re-source ~/.bashrc manually, or restart the terminal.
+  PS1='$ ' && source "$HOME"/.bashrc
+  go version || exit
+fi
+
+cd || exit
+if ! dir-ok src; then
+  mkdir -p src
+  cd src || exit
+  git clone https://github.com/jfoote/exploitable.git
+fi
+
+cd || exit
+if ! dir-ok go; then
+  mkdir -p go
+  go get -u github.com/bnagy/crashwalk/cmd/...
+  go install mvdan.cc/sh/v3/cmd/shfmt@latest
+fi
