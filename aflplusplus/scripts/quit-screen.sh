859ed21412842f8d1f2d7844ab001e92557d9cce
xlxs4 <orousoultzoglou@gmail.com>
2021-12-09 21:22:43 +0200

    Initial commit
    gitignore: only ignore the executables in root dir
    
    Initial commit
    
    afl: correctly use docker volume in README
    
    afl: highlight advantages of docker volume in README
    
    afl: Add example screenshots in README
    
    afl: Fix README pictures
    
    afl: use laf-intel for instrumentation
    
    afl: add sanitizers support
    
    afl: tell CMake to use afl-c++
    
    afl: set AR and RANLIB to llvm for lto
    
    afl: explicitly use afl-c++ instead of afl-cc
    
    afl: add todos in README
    
    Tell CMake to add STATIC libraries
    
    afl: Make dummy test crashing
    
    cmake: remove STATIC
    
    afl: Tailor dummy test crash behavior
    
    afl: Add afl-cmin shellscripts
    
    afl: afl-cmin script fixes
    
    afl: Add resources in README
    
    afl: use multiple fuzzer instances
    
    afl: helper scripts tideup
    
    afl: add shell directive, wrap mkdir/cd
    
    afl: send ctrl+c to screen before killing it
    
    afl: document helper scripts
    
    afl: Add ToC in README
    
    afl: Add Using section in README
    
    afl: Add TL;DR section in README
    
    afl: use different script to abort afl-fuzz and to kill screen sessions
    
    afl: Explain shell scripts in README
    
    afl: fix stop-fuzz.sh
    
    afl: Add how to attach/detach to screen in README
    
    afl: Embed TL;DR asciinema recording in README
    
    afl: Better asciinema rec in README
    
    afl: Use afl-tmin to minimize initial testcases
    
    afl: document afl-tmin helper script
    
    afl: untangle commands in helper scripts
    
    afl: Parallelize afl-tmin
    
    afl: Split instrumentation and fuzzing in separate scripts
    
    afl: Support afl-tmin on the afl-cmin minimized corpus
    
    afl: Use cp instead of rsync in re-tmin
    
    To avoid a nasty nasty _resume directory cleanup failed bug that I caused because I'm an idiot and it's late.
    Here is what generates the error in case you were interested:
    https://github.com/mirrorer/afl/blob/master/afl-fuzz.c#L3581
    
    afl: Mention screen caveat in README
    
    afl: Add clean script
    
    afl: Record final asciinema cast
    
    afl: Use function to install packages in setup.sh
    
    afl: Add GDB and Go dependencies
    
    afl: Setup exploitable GDB pluging and crashwalk
    
    afl: Use cwtriage and cwdump to triage found crashes
    
    afl: Add TODO roadmap in README
    
    afl: Remove obsolete instrumentation section from README
    
    afl: Update clean.sh
    
    afl: Don't return fuzz in main; prepare persistent mode
    
    afl: Parse cleanup
    Thanks to template argument deduction and uniform initialization
    
    afl: Deferred initialization
    
    afl: Implement persistent mode
    
    afl: Add shared memory fuzzing TODO
    
    afl: Create separate .gitignore for afl++ dir
    
    afl: Collapse ToC in README
    
    afl: Move scripts to scripts directory
    
    afl: Update scripts path in README
    
    afl: Update asciinema rec
    
    Disable deferred initialization and persistent mode
    
    Move clean script to util directory
    
    Manually install go
    
    EditorConfig: shell configuration
    
    Add shell format utility script
    
    Format shell scripts
    
    Make formatting shellscript executable
    
    README: Mention script caveat
    
    readme: Add more TODOs
    
    afl: Add GitLab TOC in README
    
    afl: Remove redundant CXX var from instrument
    
    afl: README typo fix
    
    afl: README alignment fix
    
    afl: rename re-tmin to retmin
    
    afl: Use bash interpreter from env in shell directives
    
    afl: remove && from quitting screen session commands
    
    afl: Use screen -xr instead of -r
    
    afl: Mention manually sourcing ~/.bashrc
    
    afl: Correctly add go to PATH in setup.sh
    
    afl: Set GOPATH; add it to PATH
    
    afl: Link to main afl++ README in README
    
    afl: Mention iterative fuzzing in README
    
    afl: Mention setup.sh works inside container in README
    
    afl: remove apt autoremove from setup.sh
    
    afl: add progress bar in rsync call
    
    afl: Add RelWithDebInfo in instrument.sh
    
    afl: Remove TODOs section from README
    
    afl: Mention sourcing .bashrc in README
    
    afl: Mention GNU parallel in tmin
    
    afl: Mention screen -dr in README
    
    afl: Ensure running scripts from aflplusplus/

---
+++ aflplusplus/scripts/quit-screen.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Kill the running screen sessions.
+
+DESIRED_PATH="aflplusplus"
+[[ "${PWD##*/}" == "$DESIRED_PATH" ]] || { echo "Run from $DESIRED_PATH"; exit; }
+
+screen -X -S fuzzer1 quit
+screen -X -S fuzzer2 quit
+screen -X -S tmin quit
+screen -X -S cmin quit
+screen -X -S crashwalk quit
