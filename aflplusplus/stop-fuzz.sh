f8cfff8c491091abed564bd9c5ace72cf231bedf
xlxs4 <orousoultzoglou@gmail.com>
2021-12-14 20:48:22 +0000

    afl: send ctrl+c to screen before killing it

--- aflplusplus/stop-fuzz.sh
+++ aflplusplus/stop-fuzz.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-screen -X -S fuzzer1 quit \
+screen -r "fuzzer1" -X stuff $'\003\n' \
+&& screen -r "fuzzer1" -X stuff $'\003\n' \
+&& screen -X -S fuzzer1 quit \
 && screen -X -S fuzzer2 quit
