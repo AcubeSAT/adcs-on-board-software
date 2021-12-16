21c1d8c0e9033bd4e14528addedbe7f6f7a28a85
xlxs4 <orousoultzoglou@gmail.com>
2021-12-16 23:44:00 +0000

    afl: Add clean script

---
+++ aflplusplus/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Remove the folder containing the instrumented executable,
+# all `afl-fuzz`-generated files, including what the fuzzing
+# instances found, and the minimized initial input testcases.
+
+rm -rf build findings minimized
