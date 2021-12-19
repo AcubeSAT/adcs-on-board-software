b1abbbbe4632556c6e59814fcf2d21ee15d80f97
xlxs4 <orousoultzoglou@gmail.com>
2021-12-19 18:36:53 +0000

    afl: Setup exploitable GDB pluging and crashwalk

--- aflplusplus/setup.sh
+++ aflplusplus/setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# Make sure all dependencies are installed and ready to go.
+
 # Check if package is installed, install it otherwise.
 function maybe-install {
     if ! dpkg-query -W -f='${Status}' "$1" | grep "ok installed"
     then
         apt install --yes --no-install-recommends "$1"
+    fi
+}
+
+# Check if directory exists and is not empty.
+function dir-ok {
+    if [[ -d "$1" && -n "$(ls -A "$1")" ]]
+    then
+        true
+    else
+        false
     fi
 }
 
@@ -15,3 +27,19 @@ apt update \
 && maybe-install rsync \
 && maybe-install gdb \
 && maybe-install golang
+
+cd || exit
+if ! dir-ok src
+then
+    mkdir -p src
+    cd src || exit
+    git clone https://github.com/jfoote/exploitable.git
+fi
+
+cd || exit
+if ! dir-ok go
+then
+    mkdir -p go
+    export GOPATH=~/go
+    go get -u github.com/bnagy/crashwalk/cmd/...
+fi
