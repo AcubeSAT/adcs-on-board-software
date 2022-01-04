e81b880df88a26535a5471f74b10062fe5746881
kostas <kzgoulas@gmail.com>
2022-01-04 22:42:41 +0200

    correct_tests added

--- test/SunPositionTest.cpp
+++ test/SunPositionTest.cpp
@@ -6,11 +6,11 @@ using namespace Eigen;
 
 
 TEST_CASE("Sun Position Test")
+
 {
-    double time = 2.4601;
+    double time = 2460127.50000000;
     Vector3d sunpos = sun_position(time);
     REQUIRE(sunpos(0) == Approx(-0.174717604923527));
     REQUIRE(sunpos(1) == Approx(0.918928406982493));
     REQUIRE(sunpos(2) == Approx(0.398346002571225));
-
 }
