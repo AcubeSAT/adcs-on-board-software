35ccb9f178608d2790b1a7cfef2ffe4f05e9e0da
kostas <kzgoulas@gmail.com>
2022-01-12 13:31:50 +0200

    add one more test

--- test/SunPositionTest.cpp
+++ test/SunPositionTest.cpp
@@ -5,7 +5,7 @@
 using namespace Eigen;
 
 
-TEST_CASE("Sun Position Test")
+TEST_CASE("Sun Position Test1")
 
 {
     double time = 2460127.50000000;
@@ -13,4 +13,13 @@ TEST_CASE("Sun Position Test")
     REQUIRE(sunpos(0) == Approx(-0.174717604923527));
     REQUIRE(sunpos(1) == Approx(0.918928406982493));
     REQUIRE(sunpos(2) == Approx(0.398346002571225));
+}
+TEST_CASE("Sun Position Test2")
+
+{
+    double time = 2.4601;
+    Vector3d sunpos = sun_position(time);
+    REQUIRE(sunpos(0) == Approx(-0.407105226684744));
+    REQUIRE(sunpos(1) == Approx(-0.828752263583653));
+    REQUIRE(sunpos(2) == Approx(-0.374407193426776));
 }
