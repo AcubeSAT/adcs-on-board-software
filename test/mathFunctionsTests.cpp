647b2e083099f8b032aafba12d425afccc507246
kostas <kzgoulas@gmail.com>
2022-02-04 21:36:47 +0200

    Correct math functions tests

--- test/mathFunctionsTests.cpp
+++ test/mathFunctionsTests.cpp
@@ -238,9 +238,9 @@ TEST_CASE("EciToEcef Test1")
     double gstime = 4.883067536994346;
     Vector3f vec = eci2ecef(vecECI, gstime);
 
-    REQUIRE(vec[0] == Approx(218.984818755685 * 1000));
-    REQUIRE(vec[1] == Approx(-6870.05920017860 * 1000));
-    REQUIRE(vec[2] == Approx(-14.6876088752119 * 1000));
+    REQUIRE(vec[0] == Approx(218.984818755685));
+    REQUIRE(vec[1] == Approx(-6870.05920017860));
+    REQUIRE(vec[2] == Approx(-14.6876088752119));
 
 }
 
@@ -250,9 +250,9 @@ TEST_CASE("EciToEcef Test2")
     double gstime = 3.3453224;
     Vector3f vec = eci2ecef(vecECI, gstime);
 
-    REQUIRE(vec[0] == Approx(2.25243318591924 * 1000));
-    REQUIRE(vec[1] == Approx(-0.465343682636487 * 1000));
-    REQUIRE(vec[2] == Approx(1212 * 1000));
+    REQUIRE(vec[0] == Approx(2.25243318591924));
+    REQUIRE(vec[1] == Approx(-0.465343682636487));
+    REQUIRE(vec[2] == Approx(1212));
 
 }
 
@@ -262,9 +262,9 @@ TEST_CASE("EciToEcef Test3")
     double gstime = 12234909;
     Vector3f vec = eci2ecef(vecECI, gstime);
 
-    REQUIRE(vec[0] == Approx(-74334.9817031508 * 1000));
-    REQUIRE(vec[1] == Approx(-807.327158822088 * 1000));
-    REQUIRE(vec[2] == Approx(0.143200000000000 * 1000));
+    REQUIRE(vec[0] == Approx(-74334.9817031508));
+    REQUIRE(vec[1] == Approx(-807.327158822088));
+    REQUIRE(vec[2] == Approx(0.143200000000000));
 
 }
 
