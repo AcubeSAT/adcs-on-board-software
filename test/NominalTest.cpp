3b848799bf5f396eab9d3886d7f0649e1654ac8d
kostas <kzgoulas@gmail.com>
2022-05-11 13:53:28 +0300

    Add test for Nominal function

--- test/NominalTest.cpp
+++ test/NominalTest.cpp
@@ -2,11 +2,35 @@
 #include "Nominal.hpp"
 
 using namespace Eigen;
-
+const SatelliteModel satelliteModel;
 TEST_CASE("First part of Nominal test time step 11") {
     MeasurementVector measurments(-0.946772755370904, -0.141294636304082, -0.289235501692104,-0.413443195401665,0.862767093417922,-0.291028635517626);
-
     Vector3f gyroscopeBias = {0, 0, 0};
+
+    Matrix<float, LocalStateSize, LocalStateSize> Q;
+    Q << 0.000100000000000000,	0,	0,	0,	0,	0,
+    0,	0.000100000000000000,	0,	0,	0,	0,
+    0,	0,	0.000100000000000000,	0,	0,	0,
+    0,	0,	0,	1.00000000000000e-07,	0,	0,
+    0,	0,	0,	0,	1.00000000000000e-07,	0,
+    0,	0,	0,	0,	0,	1.00000000000000e-07;
+
+    Matrix<float, MeasurementSize, MeasurementSize> R;
+    R << 0.000500000000000000	,0,	0,	0,	0,	0,
+    0,	0.000500000000000000,	0,	0,	0,	0,
+    0,	0,	0.000500000000000000,	0,	0,	0,
+    0,	0,	0,	0.00100000000000000,	0,	0,
+    0,	0,	0,	0,	0.00100000000000000,	0,
+    0,	0,	0,	0,	0,	0.00100000000000000;
+
+    Matrix<float, LocalStateSize, LocalStateSize> P;
+    P
+            << 0.00136699495713899, -0.000114760060275635, 0.000256861463917196, 0, 0, 0,
+            -0.000114760060275612, 0.000441521240017950, -7.67733697177619e-06, 0, 0,	0,
+            0.000256861463917308,	-7.67733697184630e-06, 0.000401588226538019, 0,	0, 0,
+            0, 0,	0, 1, 0, 0,
+            0, 0,	0, 0, 1, 0,
+            0, 0,	0, 0, 0, 1;
 
     OrbitalParameters orbitalParameters;
     Matrix<float, 180, 288> reflectivityData1 = Matrix<float, 180, 288>::Identity() * 0;
@@ -17,41 +41,14 @@ TEST_CASE("First part of Nominal test time step 11") {
         em.ModelEnvironment();
     }
     MEKF mekf;
-    FirstPartOfNominal(em, mekf, gyroscopeBias,measurments);
+    FirstPartOfNominal(em, mekf,satelliteModel, gyroscopeBias,Q,R,P,measurments);
     auto outputState = mekf.getGlobalState();
     GlobalStateVector expectedState;
+
     expectedState
             <<0.449311355178033,-0.119669240619940,0.885255918577615,-0.0109789608264623,0,0,0;
-    REQUIRE(outputState(0) == Approx(expectedState(0)).epsilon(0.01));
-    REQUIRE(outputState(1) == Approx(expectedState(1)).epsilon(0.01));
-    REQUIRE(outputState(2) == Approx(expectedState(2)).epsilon(0.01));
-    REQUIRE(outputState(3) == Approx(expectedState(3)).epsilon(0.01));
-    REQUIRE(outputState(4) == Approx(expectedState(4)).epsilon(0.01));
-    REQUIRE(outputState(5) == Approx(expectedState(5)).epsilon(0.01));
-    REQUIRE(outputState(6) == Approx(expectedState(6)).epsilon(0.01));
-}
-TEST_CASE("First part of Nominal test time step 1") {
-    MeasurementVector measurments(-0.945805868176365, -0.140182728736128, -0.292916476640093,-0.418613482147844,0.855938538682595,-0.303532486831246);
+    for (int i = 0;i < 7; i++){
+        REQUIRE(outputState(i) == Approx(expectedState(i)).epsilon(0.01));
+    }
 
-    Vector3f gyroscopeBias = {-1, 3.141592, 2.7182};
-
-    OrbitalParameters orbitalParameters;
-    Matrix<float, 180, 288> reflectivityData1 = Matrix<float, 180, 288>::Identity() * 0;
-    orbitalParameters.calculateTime(tle6PM500, 'v', 'd', 'i', wgs84);
-    EnvironmentalModel em(orbitalParameters, reflectivityData1);
-    em.ModelEnvironment();
-    //em.ModelEnvironment();
-    MEKF mekf;
-    FirstPartOfNominal(em, mekf, gyroscopeBias,measurments);
-    auto outputState = mekf.getGlobalState();
-    GlobalStateVector expectedState;
-    expectedState
-            <<0.449400665879234,-0.117185842813310,0.885452033251265,-0.0167695128920361,-1,3.141592,2.7182;
-    REQUIRE(outputState(0) == Approx(expectedState(0)).epsilon(0.01));
-    REQUIRE(outputState(1) == Approx(expectedState(1)).epsilon(0.01));
-    REQUIRE(outputState(2) == Approx(expectedState(2)).epsilon(0.01));
-    REQUIRE(outputState(3) == Approx(expectedState(3)).epsilon(0.01));
-    REQUIRE(outputState(4) == Approx(expectedState(4)).epsilon(0.01));
-    REQUIRE(outputState(5) == Approx(expectedState(5)).epsilon(0.01));
-    REQUIRE(outputState(6) == Approx(expectedState(6)).epsilon(0.01));
 }
