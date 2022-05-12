6b683dc66a6bdbdcbe41185f5075da7d3fddaf36
kostas <kzgoulas@gmail.com>
2022-05-12 13:07:26 +0300

    Add documentation

--- inc/Nominal.hpp
+++ inc/Nominal.hpp
@@ -4,11 +4,14 @@
 #include "MEKF.hpp"
 
 /**
- * First Part of Nominal (until MEKF set global state.
+ * Main function for Nominal Mode
  * @param em Environmental Model Class's Object
  * @param mekf MEKF Class's Object
- * @param quaternionECIBody Quaternion from ECI frame to Body frame
- * @param magneticBody Magnetic field in Body frame
+ * @param satelliteModel Object of the Class SatelliteModel that implements the space environment
  * @param gyroscopeBias Gyroscope's Bias
+ * @param Q Process noise covariance Matrix
+ * @param R Measurement noise covariance Matrix
+ * @param P Estimation error covariance Matrix
+ * @param measurements Vector with the measurements of magnetic field vector and sun vector in the first frame, in our case Body
  */
 void FirstPartOfNominal(EnvironmentalModel em,MEKF &mekf,const SatelliteModel &satelliteModel,Eigen::Vector3f gyroscopeBias,Eigen::Matrix<float, LocalStateSize, LocalStateSize> Q,Eigen::Matrix<float, MeasurementSize, MeasurementSize> R,Eigen::Matrix<float, LocalStateSize, LocalStateSize> P,MeasurementVector measurements);
