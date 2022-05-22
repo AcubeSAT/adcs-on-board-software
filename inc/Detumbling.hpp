19053fd7a3c7dd88a23f22c623f36eb9f1386b97
sourland <jem2010@windowslive.com>
2022-05-22 23:01:30 +0300

    implement detumbling mode function

--- inc/Detumbling.hpp
+++ inc/Detumbling.hpp
@@ -4,20 +4,29 @@
 #include "Eigen/Geometry"
 #include "MathFunctions.hpp"
 #include "EnvironmentalModel.hpp"
+#include "Parameters.hpp"
 
 /**
-The purpose of the AOCS detumbling mode is to autonomously dissipate and control
-the satellite's angular velocity. This is achieved with a B-dot controller directly using
-measurements from the high precision 3-axis magnetometer. In case of magnetometer
-failure, the redundant 3-axis magnetometer unit integrated in the iMTQ board is used.
-Actuation is performed solely by the magnetorquers. No satellite orbit propagation,
-initialized by a TLE file, is performed during this mode. In order to perform the actuation, it is necessary to
-calculate the desired magnetic dipole from the B-dot controller and then calculate the required torque to detumble
-the AcubeSAT nanosatellite.
-*/
+ * The purpose of the AOCS detumbling mode is to autonomously dissipate and control
+ * the satellite's angular velocity. This is achieved with a B-dot controller directly using
+ * measurements from the high precision 3-axis magnetometer. In case of magnetometer
+ * failure, the redundant 3-axis magnetometer unit integrated in the iMTQ board is used.
+ * Actuation is performed solely by the magnetorquers. No satellite orbit propagation,
+ * initialized by a TLE file, is performed during this mode. In order to perform the actuation, it is necessary to
+ * calculate the desired magnetic dipole from the B-dot controller and then calculate the required torque to detumble
+ * the AcubeSAT nanosatellite.
+ * @param bDot a B-dot controller to calculate the desired magnetic dipole
+ * @param ECIToBodyQuaternion the quaternion between ECI and Body frame
+ * @param currentAngularVelocity the current angular velocity of the satellite
+ * @param environmentalModel a model of the environment around the AcubeSAT
+ * @param isDone a parameter that indicates if detumbling is done or not
+ */
 
+void Detumbling(Bdot &bDot, Eigen::Quaternionf &ECIToBodyQuaternion, Eigen::Vector3f &currentAngularVelocity,
+                EnvironmentalModel &environmentalModel, bool &isDone);
 
 /**
+ * Calculate the desired magnetic pole using a bdot controller
  * @param bDot a B-dot controller to calculate the desired magnetic dipole
  * @returns desired torque value for magnetorquer actuation
  */
@@ -25,8 +34,31 @@ Eigen::Vector3f calculateDesiredMagneticTorque(Bdot &bDot);
 
 /**
  * Estimate the current angular velocity of the satellite.
- * @param bDot a B-dot controller to calculate the angular velocity
- * @param magneticFieldBody the first magnetic field measurement of B-dot sampled during sampling interval in a control cycle
+ * @param bDotVector the calculated magnetic field derivative vector of the Bdot controller in a single control cycled
+ * @param magneticFieldBody the first magnetic field measurement of B-dot measured during sampling interval in a control cycle
  * @return angular velocity estimation expressed in body frame
  */
 Eigen::Vector3f estimateAngularVelocity(const Eigen::Vector3f bDotVector, const Eigen::Vector3f magneticFieldBody);
+
+/**
+ * Update the coefficients of a non-constant quaternion, in this case the ECI frame to Body frame quaternion
+ * with respect to the quaternion kinematic model.
+ * @param quaternion the quaternion to be updated
+ * @param angularVelocity the angular velocity of a coordinate system (frame), in this case the angular velocity of the
+ * AcubeSAT nanosatellite expressed in the body frame
+ * @param timeInterval the duration of the update
+ */
+void updateQuaternion(Eigen::Quaternionf &quaternion, const Eigen::Vector3f angularVelocity, const float timeInterval);
+
+/**
+ * Actuate by using the magnetorquers to apply the desired torque calculated from previous steps, and reduce the angular
+ * velocity of the AcubeSAT nanosatellite.
+ * @param angularVelocity the current angular velocity of the satellite
+ * @param quaternion
+ * @param desiredMagneticTorque
+ * @param timestep
+ * @param actuationTime
+ */
+void
+actuate(Eigen::Vector3f &angularVelocity, Eigen::Quaternionf &quaternion, const Eigen::Vector3f desiredMagneticTorque,
+        const float timestep, const float actuationTime);
--- test/DetumblingTest.cpp
+++ test/DetumblingTest.cpp
@@ -43,61 +43,17 @@ TEST_CASE("Detumbling Case 1") {
     Quaternionf q_body_eci({0.3757, 0.5983, -0.2364, -0.6671});
     Vector3f omega = {PI / 30, PI / 30, PI / 30};
     Vector3f omega_dot = {0, 0, 0};
-    Matrix<float, 3, 3> Inertia({
-                                        {0.0412158223637031,    -1.85801942968305e-06, 0.00137245665256380},
-                                        {-1.85801942968300e-06, 0.0410190161501943,    -1.64802792093441e-05},
-                                        {0.00137245665256380,   -1.64802792093441e-05, 0.00695520282610255}
-                                });
-    Matrix<float, 3, 3> I = Matrix<float, 3, 3>::Identity();
-    double dt = 0.1;
-    bool switchToNominal = false;
-    Vector3f magneticFieldBody1 = {0, 0, 0};
-    Vector3f magneticFieldBody2 = {0, 0, 0};
-    Vector3f desiredTorque = {0, 0, 0};
-    Vector3f w_estim = {100, 100, 100};
-    Quaternionf q_dot = {0, omega.x(), omega.y(), omega.z()};
-    float trigger = 0.035;
-    int stepsIsStable = 0;
-    while (stepsIsStable < 10) {
-        em.ModelEnvironment();
-        magneticFieldBody1 = rotateVector(q_body_eci, em.getMagneticField());
-        bdot.setSamplingBeginMagneticFieldBody(magneticFieldBody1);
-        if (q_dot.norm() > 1e-100) {
-            q_dot = quaternionProduct(q_body_eci, q_dot);
-            q_dot.coeffs() = q_dot.coeffs() * 0.5;
-            q_body_eci.coeffs() += q_dot.coeffs() * dt;
-            q_body_eci.normalize();
+
+    bool isDone = false;
+    int stableTime = 0;
+    while (stableTime < 10) {
+        Detumbling(bdot, q_body_eci, omega, em, isDone);
+        if (isDone) {
+            stableTime++;
         }
-        em.ModelEnvironment();
-        magneticFieldBody2 = rotateVector(q_body_eci, em.getMagneticField());
-        bdot.setSamplingEndMagneticFieldBody(magneticFieldBody2);
-        desiredTorque = calculateDesiredMagneticTorque(bdot);
-
-
-        w_estim = estimateAngularVelocity(bdot.getBDotVector(), bdot.getSamplingBeginMagneticFieldBody());
-
-
-        // ACTUATE
-
-        for (int i = 0; i < 7; ++i) {
-            em.ModelEnvironment();
-        }
-
-        omega = 7 * 0.5 * omega * dt +
-                0.5 * Inertia.inverse() * (desiredTorque - omega.cross(Inertia * omega)) * (0.7 * dt) * (0.7 * dt);
-        q_dot = {0, omega.x(), omega.y(), omega.z()};
-        if (q_dot.norm() > 1e-100) {
-            q_dot.normalize();
-            q_body_eci = quaternionProduct(q_body_eci, q_dot);
-            q_body_eci.normalize();
-        }
-
-        switchToNominal = ((abs(w_estim.x()) < trigger) && (abs(w_estim.y()) < trigger) &&
-                           (abs(w_estim.z()) < trigger));
-        if (switchToNominal) stepsIsStable++;
-
     }
-    REQUIRE(!(stepsIsStable < 10));
+
+    REQUIRE(!(stableTime < 10));
 }
 
 
@@ -111,61 +67,16 @@ TEST_CASE("Detumbling Case 2") {
     Quaternionf q_body_eci({0.71772, 0.449563, 0.177631, 0.501259});
     Vector3f omega = {PI / 3, PI / 3, PI / 3};
     Vector3f omega_dot = {0, 0, 0};
-    Matrix<float, 3, 3> Inertia({
-                                        {0.0412158223637031,    -1.85801942968305e-06, 0.00137245665256380},
-                                        {-1.85801942968300e-06, 0.0410190161501943,    -1.64802792093441e-05},
-                                        {0.00137245665256380,   -1.64802792093441e-05, 0.00695520282610255}
-                                });
-    Matrix<float, 3, 3> I = Matrix<float, 3, 3>::Identity();
-    double dt = 0.1;
-    bool switchToNominal = false;
-    Vector3f magneticFieldBody1 = {0, 0, 0};
-    Vector3f magneticFieldBody2 = {0, 0, 0};
-    Vector3f desiredTorque = {0, 0, 0};
-    Vector3f w_estim = {100, 100, 100};
-    Quaternionf q_dot = {0, omega.x(), omega.y(), omega.z()};
-    float trigger = 0.035;
-    int stepsIsStable = 0;
 
-    while (stepsIsStable < 10) {
-        em.ModelEnvironment();
-        magneticFieldBody1 = rotateVector(q_body_eci, em.getMagneticField());
-        bdot.setSamplingBeginMagneticFieldBody(magneticFieldBody1);
-        if (q_dot.norm() > 1e-100) {
-            q_dot = quaternionProduct(q_body_eci, q_dot);
-            q_dot.coeffs() = q_dot.coeffs() * 0.5;
-            q_body_eci.coeffs() += q_dot.coeffs() * dt;
-            q_body_eci.normalize();
+    bool isDone = false;
+    int stableTime = 0;
+    while (stableTime < 10) {
+        Detumbling(bdot, q_body_eci, omega, em, isDone);
+        if (isDone) {
+            stableTime++;
         }
-        em.ModelEnvironment();
-        magneticFieldBody2 = rotateVector(q_body_eci, em.getMagneticField());
-        bdot.setSamplingEndMagneticFieldBody(magneticFieldBody2);
-        desiredTorque = calculateDesiredMagneticTorque(bdot);
-
-
-        w_estim = estimateAngularVelocity(bdot.getBDotVector(), bdot.getSamplingBeginMagneticFieldBody());
-
-
-        // ACTUATE
-
-        for (int i = 0; i < 7; ++i) {
-            em.ModelEnvironment();
-        }
-
-        omega = 7 * 0.5 * omega * dt +
-                0.5 * Inertia.inverse() * (desiredTorque - omega.cross(Inertia * omega)) * (0.7 * dt) * (0.7 * dt);
-        q_dot = {0, omega.x(), omega.y(), omega.z()};
-        if (q_dot.norm() > 1e-100) {
-            q_dot.normalize();
-            q_body_eci = quaternionProduct(q_body_eci, q_dot);
-            q_body_eci.normalize();
-        }
-
-        switchToNominal = ((abs(w_estim.x()) < trigger) && (abs(w_estim.y()) < trigger) &&
-                           (abs(w_estim.z()) < trigger));
-        if (switchToNominal) stepsIsStable++;
-
     }
 
-    REQUIRE(!(stepsIsStable < 10));
+    REQUIRE(!(stableTime < 10));
+
 }
