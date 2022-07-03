292ef8157fcf3d6f5b056652cc81f54f551a43a4
kostas <kzgoulas@gmail.com>
2022-07-03 20:41:53 +0300

    Add test for measurement function

--- src/MeasurmentsForNominal.cpp
+++ src/MeasurmentsForNominal.cpp
@@ -12,14 +12,13 @@ MeasurmentsProduction(Vector3f sunPositionECI, Vector3f satellitePositionECI, fl
             0, 0.0014, 0,
             0, 0, 0.0014;
 
-    Vector<float, 7> xZero(-0.449279850250564, 0.118894667693728, -0.885360292481303, 0.0121994528668081, 0, 0, 0);
-    //SunBody
     Quaternionf quaternionSunECI = {0, sunPositionECI[0], sunPositionECI[1], sunPositionECI[2]};
     Quaternionf quaternionECItoBody = {-0.449279850250564, 0.118894667693728, -0.885360292481303, 0.0121994528668081};
     Quaternionf quaternionECItoBodyConjugate = {-0.449279850250564, -0.118894667693728, 0.885360292481303,
                                                 -0.0121994528668081};
     for (int i = 0; i < 3; i++) {
         sunPositionECI[i] = sunPositionECI[i] / sunPositionECI.norm();
+        satellitePositionECI[i] = satellitePositionECI[i] / satellitePositionECI.norm();
     }
     Quaternionf temp = quaternionProduct(quaternionECItoBodyConjugate,
                                          quaternionProduct(quaternionSunECI, quaternionECItoBody));
@@ -28,7 +27,7 @@ MeasurmentsProduction(Vector3f sunPositionECI, Vector3f satellitePositionECI, fl
     sunPositionBody[1] = temp.y();
     sunPositionBody[2] = temp.z();
     for (int i = 0; i < 3; i++) {
-        satellitePositionECI[i] = satellitePositionECI[i] / satellitePositionECI.norm();
+        sunPositionBody[i] = sunPositionBody[i] / sunPositionBody.norm();
     }
     Quaternionf quaternionSatelliteECI = {0, satellitePositionECI[0], satellitePositionECI[1], satellitePositionECI[2]};
     Quaternionf temp2 = quaternionProduct(quaternionECItoBodyConjugate,
@@ -38,17 +37,17 @@ MeasurmentsProduction(Vector3f sunPositionECI, Vector3f satellitePositionECI, fl
     satellitePositionBody[1] = temp.y();
     satellitePositionBody[2] = temp.z();
 
-    Vector3f totalSun = cssCompensation(sunPositionBody, quaternionECItoBody, satellitePositionECI, albedo);
-
     Vector3f magneticBody = rotateVector(quaternionECItoBody, magneticFieldECI);
-    magneticBody = magneticBody + R * magneticBody;
+    Vector3f randomVector = {rand(),rand(),rand()};
+    magneticBody = magneticBody + R * randomVector;
     for (int i = 0; i < 3; i++) {
         magneticBody[i] = magneticBody[i] / magneticBody.norm();
     }
 
     Vector3f gyroscopeMeasurements = {0, 0, 0};
 
-    measurements = {totalSun[0], totalSun[1], totalSun[2], magneticBody[0], magneticBody[1], magneticBody[2],
+    measurements = {sunPositionBody[0], sunPositionBody[1], sunPositionBody[2], magneticBody[0], magneticBody[1],
+                    magneticBody[2],
                     gyroscopeMeasurements[0], gyroscopeMeasurements[1], gyroscopeMeasurements[2]};
 
     return measurements;
---
+++ test/MeasurementsForNominalTest.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch.hpp>
+#include "MeasurmentsForNominal.hpp"
+
+using namespace Eigen;
+
+TEST_CASE("Measurements for nominal test") {
+    Vector<float, 9> measurements;
+    Vector3f magneticFieldECI = {7740.17871171431, 1088.51463852010, 21943.9176853079};
+    Vector3f sunPositionECI = {-0.171851379656845, 0.903853476098922, 0.391811175254017};
+    Vector3f satellitePositionECI = {-0.979555954434493,-0.201160548089278,-0.00213682576508476};
+    float albedo = 0;
+    measurements = MeasurmentsProduction(sunPositionECI, satellitePositionECI, albedo, magneticFieldECI);
+    Vector<float, 9> res = {-0.413148384628908, 0.862004142487583, -0.293695881164122, -0.946661971565983,
+                            -0.140309616201384, -0.293181612221257, 0, 0, 0};
+    for (int i = 0; i < 3; i++) {
+        REQUIRE(measurements[i] == Approx(res[i]).epsilon(0.01));
+    }
+}
