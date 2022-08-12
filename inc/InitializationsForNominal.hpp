330fa3fe8660b120a8302eee761cafeb18935647
kongr45gpen <electrovesta@gmail.com>
2022-08-12 10:18:15 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- inc/InitializationsForNominal.hpp
+++ inc/InitializationsForNominal.hpp
@@ -6,11 +6,11 @@
  * A structure that contains a reflectivity data matrix, an estimation error covariance
  * matrix and an OrbitalParameters class item
  */
-typedef struct {
+struct PAndReflectivityData {
     Eigen::Matrix<float, 180, 288> reflectivityData;
     Eigen::Matrix<float, LocalStateSize, LocalStateSize> P;
     OrbitalParameters orbitalParameters;
-} PAndReflectivityData;
+};
 /**
  * A function that initialize parameters needed in Nominal function
  * @return reflectivityData, estimation error covariance Matrix and an OrbitalParameters class item
