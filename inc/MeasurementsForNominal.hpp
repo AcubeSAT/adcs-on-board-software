19f22847481bd36abfe055ba856784750064d878
Vasiliki Pappa <valia.pp8@gmail.com>
2022-08-19 22:37:59 +0300

    Move Nominal Measurements in tests

--- inc/MeasurementsForNominal.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Eigen/Geometry"
-#include "EnvironmentalModel.hpp"
-#include "MEKF.hpp"
-#include "Parameters.hpp"
-
-Eigen::Vector<float,9> MeasurmentsProduction(Eigen::Vector3f sunPositionECI,Eigen::Vector3f satellitePositionECI,float albedo,Eigen::Vector3f magneticFieldECI);
--- test/MeasurementsForNominal.hpp
+++ test/MeasurementsForNominal.hpp
@@ -1,50 +1,49 @@
-#include "MeasurementsForNominal.hpp"
+#pragma once
+
+#include "Eigen/Geometry"
 #include "MathFunctions.hpp"
-#include "CSSCompensation.hpp"
 
-using namespace Eigen;
-
-Vector<float, 9>
-MeasurmentsProduction(Vector3f sunPositionECI, Vector3f satellitePositionECI, float albedo, Vector3f magneticFieldECI) {
-    Vector<float, 9> measurements;
-    Matrix<float, 3, 3> R;
+Eigen::Vector<float, 9>
+MeasurementsForNominal(Eigen::Vector3f sunPositionECI, Eigen::Vector3f satellitePositionECI, float albedo, Eigen::Vector3f magneticFieldECI) {
+    Eigen::Vector<float, 9> measurements;
+    Eigen::Matrix<float, 3, 3> R;
     R << 0.0014, 0, 0,
             0, 0.0014, 0,
             0, 0, 0.0014;
 
-    Quaternionf quaternionSunECI = {0, sunPositionECI[0], sunPositionECI[1], sunPositionECI[2]};
-    Quaternionf quaternionECItoBody = {-0.449279850250564, 0.118894667693728, -0.885360292481303, 0.0121994528668081};
-    Quaternionf quaternionECItoBodyConjugate = {-0.449279850250564, -0.118894667693728, 0.885360292481303,
+    Eigen::Quaternionf quaternionSunECI = {0, sunPositionECI[0], sunPositionECI[1], sunPositionECI[2]};
+    Eigen::Quaternionf quaternionECItoBody = {-0.449279850250564, 0.118894667693728, -0.885360292481303, 0.0121994528668081};
+    Eigen::Quaternionf quaternionECItoBodyConjugate = {-0.449279850250564, -0.118894667693728, 0.885360292481303,
                                                 -0.0121994528668081};
     for (int i = 0; i < 3; i++) {
         sunPositionECI[i] = sunPositionECI[i] / sunPositionECI.norm();
         satellitePositionECI[i] = satellitePositionECI[i] / satellitePositionECI.norm();
     }
-    Quaternionf temp = quaternionProduct(quaternionECItoBodyConjugate,
+    Eigen::Quaternionf temp = quaternionProduct(quaternionECItoBodyConjugate,
                                          quaternionProduct(quaternionSunECI, quaternionECItoBody));
-    Vector3f sunPositionBody;
+    Eigen::Vector3f sunPositionBody;
     sunPositionBody[0] = temp.x();
     sunPositionBody[1] = temp.y();
     sunPositionBody[2] = temp.z();
     for (int i = 0; i < 3; i++) {
         sunPositionBody[i] = sunPositionBody[i] / sunPositionBody.norm();
     }
-    Quaternionf quaternionSatelliteECI = {0, satellitePositionECI[0], satellitePositionECI[1], satellitePositionECI[2]};
-    Quaternionf temp2 = quaternionProduct(quaternionECItoBodyConjugate,
+    Eigen::Quaternionf quaternionSatelliteECI = {0, satellitePositionECI[0], satellitePositionECI[1], satellitePositionECI[2]};
+    Eigen::Quaternionf temp2 = quaternionProduct(quaternionECItoBodyConjugate,
                                           quaternionProduct(quaternionSatelliteECI, quaternionECItoBody));
-    Vector3f satellitePositionBody;
+    Eigen::Vector3f satellitePositionBody;
     satellitePositionBody[0] = temp.x();
     satellitePositionBody[1] = temp.y();
     satellitePositionBody[2] = temp.z();
 
-    Vector3f magneticBody = rotateVector(quaternionECItoBody, magneticFieldECI);
-    Vector3f randomVector = {1,2,3};
+    Eigen::Vector3f magneticBody = rotateVector(quaternionECItoBody, magneticFieldECI);
+    Eigen::Vector3f randomVector = {1,2,3};
     magneticBody = magneticBody + R * randomVector;
     for (int i = 0; i < 3; i++) {
         magneticBody[i] = magneticBody[i] / magneticBody.norm();
     }
 
-    Vector3f gyroscopeMeasurements = {0, 0, 0};
+    Eigen::Vector3f gyroscopeMeasurements = {0, 0, 0};
 
     measurements = {sunPositionBody[0], sunPositionBody[1], sunPositionBody[2], magneticBody[0], magneticBody[1],
                     magneticBody[2],
