b53c773621a891d49f3c6b2908ec475f95620dbb
Vasiliki Pappa <valia.pp8@gmail.com>
2022-08-19 22:47:46 +0300

    Clean-up measurements

--- test/MeasurementsForNominal.hpp
+++ test/MeasurementsForNominal.hpp
@@ -15,33 +15,24 @@ MeasurementsForNominal(Eigen::Vector3f sunPositionECI, Eigen::Vector3f satellite
     Eigen::Quaternionf quaternionECItoBody = {-0.449279850250564, 0.118894667693728, -0.885360292481303, 0.0121994528668081};
     Eigen::Quaternionf quaternionECItoBodyConjugate = {-0.449279850250564, -0.118894667693728, 0.885360292481303,
                                                 -0.0121994528668081};
-    for (int i = 0; i < 3; i++) {
-        sunPositionECI[i] = sunPositionECI[i] / sunPositionECI.norm();
-        satellitePositionECI[i] = satellitePositionECI[i] / satellitePositionECI.norm();
-    }
+
+    sunPositionECI.normalize();
+    satellitePositionECI.normalize();
+
     Eigen::Quaternionf temp = quaternionProduct(quaternionECItoBodyConjugate,
                                          quaternionProduct(quaternionSunECI, quaternionECItoBody));
-    Eigen::Vector3f sunPositionBody;
-    sunPositionBody[0] = temp.x();
-    sunPositionBody[1] = temp.y();
-    sunPositionBody[2] = temp.z();
-    for (int i = 0; i < 3; i++) {
-        sunPositionBody[i] = sunPositionBody[i] / sunPositionBody.norm();
-    }
+    Eigen::Vector3f sunPositionBody  = temp.vec();
+    sunPositionBody.normalize();
+
     Eigen::Quaternionf quaternionSatelliteECI = {0, satellitePositionECI[0], satellitePositionECI[1], satellitePositionECI[2]};
     Eigen::Quaternionf temp2 = quaternionProduct(quaternionECItoBodyConjugate,
                                           quaternionProduct(quaternionSatelliteECI, quaternionECItoBody));
-    Eigen::Vector3f satellitePositionBody;
-    satellitePositionBody[0] = temp.x();
-    satellitePositionBody[1] = temp.y();
-    satellitePositionBody[2] = temp.z();
+    Eigen::Vector3f satellitePositionBody = temp.vec();
 
     Eigen::Vector3f magneticBody = rotateVector(quaternionECItoBody, magneticFieldECI);
     Eigen::Vector3f randomVector = {1,2,3};
     magneticBody = magneticBody + R * randomVector;
-    for (int i = 0; i < 3; i++) {
-        magneticBody[i] = magneticBody[i] / magneticBody.norm();
-    }
+    magneticBody.normalize();
 
     Eigen::Vector3f gyroscopeMeasurements = {0, 0, 0};
 
