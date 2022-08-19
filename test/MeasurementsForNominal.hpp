ebc76f6c79f9c32a09072bae2ef1a7a47e20b906
Vasiliki Pappa <valia.pp8@gmail.com>
2022-08-20 00:52:38 +0300

    Optimize measurement calculation using rotateVector function

--- test/MeasurementsForNominal.hpp
+++ test/MeasurementsForNominal.hpp
@@ -5,32 +5,20 @@
 
 Eigen::Vector<float, 6>
 MeasurementsForNominal(Eigen::Vector3f sunPositionECI, Eigen::Vector3f satellitePositionECI, float albedo, Eigen::Vector3f magneticFieldECI) {
-    Eigen::Vector<float, 6> measurements;
-    Eigen::Matrix<float, 3, 3> R;
+    MeasurementVector measurements;
+    Eigen::Matrix<float, VectorSize, VectorSize> R;
     R << 0.0014, 0, 0,
             0, 0.0014, 0,
             0, 0, 0.0014;
 
-    Eigen::Quaternionf quaternionSunECI = {0, sunPositionECI[0], sunPositionECI[1], sunPositionECI[2]};
     Eigen::Quaternionf quaternionECItoBody = {-0.449279850250564, 0.118894667693728, -0.885360292481303, 0.0121994528668081};
-    Eigen::Quaternionf quaternionECItoBodyConjugate = {-0.449279850250564, -0.118894667693728, 0.885360292481303,
-                                                -0.0121994528668081};
 
     sunPositionECI.normalize();
-    satellitePositionECI.normalize();
-
-    Eigen::Quaternionf temp = quaternionProduct(quaternionECItoBodyConjugate,
-                                         quaternionProduct(quaternionSunECI, quaternionECItoBody));
-    Eigen::Vector3f sunPositionBody  = temp.vec();
+    Eigen::Vector3f sunPositionBody = rotateVector(quaternionECItoBody, sunPositionECI);
     sunPositionBody.normalize();
 
-    Eigen::Quaternionf quaternionSatelliteECI;
-    quaternionSatelliteECI.w() = 0;
-    quaternionSatelliteECI.vec() = satellitePositionECI;
-
-    Eigen::Quaternionf temp2 = quaternionProduct(quaternionECItoBodyConjugate,
-                                          quaternionProduct(quaternionSatelliteECI, quaternionECItoBody));
-    Eigen::Vector3f satellitePositionBody = temp.vec();
+    satellitePositionECI.normalize();
+    Eigen::Vector3f satellitePositionBody = rotateVector(quaternionECItoBody, satellitePositionECI);
 
     Eigen::Vector3f magneticBody = rotateVector(quaternionECItoBody, magneticFieldECI);
     Eigen::Vector3f randomVector = {1,2,3};
