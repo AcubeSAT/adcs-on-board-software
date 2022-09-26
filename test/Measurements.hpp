c5cc028aa072c4e3299c8534e42dcbb766a93c0d
Vasiliki Pappa <valia.pp8@gmail.com>
2022-09-26 22:09:41 +0300

    Update measurements

--- test/Measurements.hpp
+++ test/Measurements.hpp
@@ -3,34 +3,25 @@
 #include "Eigen/Geometry"
 #include "MathFunctions.hpp"
 
+/**
+ *
+ * @param sunPositionECI
+ * @param magneticFieldECI
+ * @param satellitePositionECI
+ * @return measurements, [0: 2] magnetometer measurements, [3: 5] gyro measurements, [6: 8] sun sensor measurements
+ */
 Eigen::Vector<float, 9>
-createMeasurements(Eigen::Vector3f sunPositionECI, Eigen::Vector3f magneticFieldECI,
-                       Eigen::Vector3f satellitePositionECI) {
+getMeasurements(Eigen::Vector3f sunPositionECI, Eigen::Vector3f magneticFieldECI,
+                   Eigen::Vector3f satellitePositionECI) {
     Eigen::Vector<float, 9> measurements;
-    Eigen::Matrix<float, VectorSize, VectorSize> R;
-    R << 0.0014, 0, 0,
-            0, 0.0014, 0,
-            0, 0, 0.0014;
 
     const Eigen::Quaternionf quaternionECItoBody = {-0.449279850250564, 0.118894667693728, -0.885360292481303,
                                                     0.0121994528668081};
 
-    const Eigen::Vector3f magneticBody = rotateVector(quaternionECItoBody, magneticFieldECI);
-    const Eigen::Vector3f randomVector = {1, 2, 3};
-    Eigen::Vector3f magneticBodyWithNoise = magneticBody + R * randomVector;
-    magneticBodyWithNoise.normalize();
-
-    measurements(Eigen::seq(0, 2)) = magneticBody;
-
-    sunPositionECI.normalize();
-    Eigen::Vector3f sunPositionBody = rotateVector(quaternionECItoBody, sunPositionECI);
-    sunPositionBody.normalize();
-    measurements(Eigen::seq(3, 5)) = sunPositionBody;
-
-    satellitePositionECI.normalize();
-    Eigen::Vector3f satellitePositionBody = rotateVector(quaternionECItoBody, satellitePositionECI);
-    satellitePositionBody.normalize();
-    measurements(Eigen::seq(6, 8)) = satellitePositionBody;
+    const Eigen::DiagonalMatrix< float, VectorSize, VectorSize> R(0.001352774925847, 0.001352774925847, 0.001352774925847);
+    const auto magneticFieldBody = rotateVector(quaternionECItoBody, magneticFieldECI);
+    const auto magnetometerMeasurement = magneticFieldBody +  R * Eigen::MatrixXf::Random(VectorSize,1);
+    measurements(Eigen::seq(0, 2)) = magnetometerMeasurement;
 
     return measurements;
 }
